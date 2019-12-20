8ca65bf4af80f43765e837db59cf87f476593901
marybngozi <maryblessing.umeh@outlook.com>
2019-12-20 13:34:45 +0100

    outlined services

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 window.onscroll = function(){
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.querySelector("nav").style.backgroundColor = "#ca7709";
+    document.querySelector("#navbar_btn").style.backgroundColor = "#fff";
+    document.querySelector("#navbar_btn").style.color = "#ca7709";
+    document.querySelector("#navbarToggler1").style.backgroundColor = "transparent";
     document.querySelectorAll(".nav-link").forEach(nav => {
       nav.style.color = "#fff";
     });
   } else {
     document.querySelector("nav").style.backgroundColor = "transparent";
+    document.querySelector("#navbar_btn").style.backgroundColor = "#ca7709";
+    document.querySelector("#navbar_btn").style.color = "#fff";
+    document.querySelector("#navbarToggler1").style.backgroundColor = "#fff";
     document.querySelectorAll(".nav-link").forEach(nav => {
       nav.style.color = "#555";
     });
