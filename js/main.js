a6304d865293b3b3c73611e061ee6fd6b64e48f5
marybngozi <maryblessing.umeh@outlook.com>
2019-12-26 21:00:29 +0100

    Fixed, and set up

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 window.onscroll = function(){
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector("nav").style.backgroundColor = "#ca7709";
+    document.querySelector("nav").classList.remove("nav_bar-still");
+    document.querySelector("nav").classList.add("nav_bar-scroll");
+    document.querySelector("nav").style.paddingTop = "25px";
+
     document.querySelector("#navbar_btn").style.backgroundColor = "#fff";
     document.querySelector("#navbar_btn").style.color = "#ca7709";
     document.querySelector("#navbarToggler1").style.backgroundColor = "transparent";
@@ -8,10 +11,18 @@ window.onscroll = function(){
       nav.style.color = "#fff";
     });
   } else {
-    document.querySelector("nav").style.backgroundColor = "transparent";
+    if(document.querySelector("nav").classList.contains("nav_bar-scroll")){
+      document.querySelector("nav").classList.remove("nav_bar-scroll");
+      document.querySelector("nav").classList.add("nav_bar-still")
+    }
+    // if(document.querySelector("#navbar_btn").classList.contains("navbar_btn-scroll")){
+    //   document.querySelector("#navbar_btn").classList.remove("navbar_btn-scroll");
+    //   document.querySelector("#navbar_btn").classList.add("navbar_btn-still")
+    // }
+
     document.querySelector("#navbar_btn").style.backgroundColor = "#ca7709";
     document.querySelector("#navbar_btn").style.color = "#fff";
-    document.querySelector("#navbarToggler1").style.backgroundColor = "#fff";
+    document.querySelector("#navbarToggler1").classList.add("navbarToggler");
     document.querySelectorAll(".nav-link").forEach(nav => {
       nav.style.color = "#555";
     });
